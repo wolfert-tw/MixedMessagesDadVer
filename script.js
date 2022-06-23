3b27d717a667e1abdd091b9573a02e144f2172d8
wolfert-tw <wolfert.tw@gmail.com>
2022-06-23 09:08:09 -0600

    Complete main implementation of creating a random joke

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const dadJokes = {
+    // Each Joke consists of the following pieces:
+    /*
+        id - unique identifier for each joke
+        active - Flag noting that the joke is 'active' and can be used.
+        dadIntro - Dad's intro line before he tells his joke. Everyone knows that Dad cannot refrain from saying SOMETHING before continuing his antics.
+        lines - Array of lines for the joke. Dad's jokes can be 1-liners, an appropriate length, or (more likely) INCREDIBLY long. 
+        laugh - The EXPECTED response to the given joke. There's always an expected response.
+    */
     _jokes: [
         {id: 1, active: true, dadIntro: 'Hey kids, look over there!', 
             lines: ['Ah, there\'s one of my DEER friends.', 'He\'s out standing in his field.'], laugh: 'HAHAHA!'}, 
         {id: 2, active: true, dadIntro: 'Ok here\'s a good one for you!', 
-            lines: ['What does a shark order at McDonal\'s?', 'What?', 'A Quarter Flounder'], laugh: 'GLUBGLUBGLUB!'}, 
+            lines: ['What does a shark order at McDonald\'s?', 'What?', 'A Quarter Flounder'], laugh: 'GLUBGLUBGLUB!'}, 
         {id: 3, active: true, dadIntro: 'I had a realization yesterday...', 
             lines: ['One day I might find a large number of BDSM magazines beneath my sons bed.', 'Needless to say, spanking will be out of the question'], 
             laugh: '*slaps knee* GOOD ONE!'}
@@ -39,8 +47,14 @@ const dadJokes = {
     }
 }
 
-// console.log("test");
-// console.log(dadJokes.jokeById(1));
-// console.log(dadJokes.findMaxId());
-// console.log(dadJokes.findRandJoke());
-console.log(dadJokes.buildRandJoke());
+const randJoke = dadJokes.buildRandJoke();
+
+console.log('Mixed Messages - Dad\'s Version');
+console.log();
+console.log(randJoke.dadIntro);
+for (let i = 0; i < randJoke.lines.length; i++) {
+    console.log(randJoke.lines[i]);
+};
+console.log(randJoke.laugh);
+
+// console.log(dadJokes.buildRandJoke());
